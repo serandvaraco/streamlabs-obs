3473e98820f8318071304438a0a950d7d47131f3
Adrian Perez <adrian@adrianperez.org>
2018-11-16 13:32:29 -0800

    feat(troubleshooter): inline controls in dropped frames notice
    Add inline controls allowing to change server, edit Video and
    Audio bitrate inline after receiving (and acting upon) a "Dropped
    Frames" notification.
    
    Notice the values are saved automatically, as in most of our UI.
    Handling values separately, Stream (server) and Output (bitrates)
    is by design.

--- app/components/windows/Troubleshooter.vue.ts
+++ app/components/windows/Troubleshooter.vue.ts
@@ -4,34 +4,91 @@ import { Component } from 'vue-property-decorator';
 import { Inject } from '../../util/injector';
 import ModalLayout from '../ModalLayout.vue';
 import { TIssueCode } from 'services/troubleshooter';
-import { INotificationsServiceApi, INotification } from 'services/notifications';
-import { ISettingsServiceApi } from 'services/settings';
+import {
+  INotificationsServiceApi,
+  INotification
+} from 'services/notifications';
+import { ISettingsServiceApi, ISettingsSubCategory } from 'services/settings';
 import { WindowsService } from 'services/windows';
-
+import GenericFormGroups from 'components/obs/inputs/GenericFormGroups.vue';
+import { TObsFormData } from 'components/obs/inputs/ObsInput';
+import { EStreamingState, StreamingService } from '../../services/streaming';
 
 @Component({
-  components: { ModalLayout }
+  components: { ModalLayout, GenericFormGroups }
 })
 export default class Troubleshooter extends Vue {
-
   @Inject() private notificationsService: INotificationsServiceApi;
   @Inject() private settingsService: ISettingsServiceApi;
   @Inject() private windowsService: WindowsService;
+  @Inject() streamingService: StreamingService;
 
-  issueCode = this.windowsService.getChildWindowQueryParams().issueCode as TIssueCode;
+  streamingSettings: ISettingsSubCategory[] | null = null;
+  outputSettings: ISettingsSubCategory[] | null = null;
 
+  issueCode = this.windowsService.getChildWindowQueryParams()
+    .issueCode as TIssueCode;
 
-  get issue(): INotification {
-    return this.notificationsService.getAll().find(notify => notify.code === this.issueCode);
+  mounted() {
+    this.getSettings();
   }
 
+  get issue(): INotification {
+    return this.notificationsService
+      .getAll()
+      .find(notify => notify.code === this.issueCode);
+  }
+
+  getSettings() {
+    if (this.issueCode === 'FRAMES_DROPPED') {
+      [this.streamingSettings, this.outputSettings] = getStreamSettings(
+        this.settingsService.getSettingsFormData
+      );
+    }
+  }
 
   showSettings() {
     this.settingsService.showSettings();
   }
 
+  get isStreaming() {
+    return this.streamingService.isStreaming;
+  }
+
+  saveOutputSettings() {
+    this.settingsService.setSettings('Output', this.outputSettings);
+  }
+
+  saveStreamingSettings() {
+    this.settingsService.setSettings('Stream', this.streamingSettings);
+  }
 
   moment(time: number): string {
     return moment(time).fromNow();
   }
 }
+
+const getStreamSettings = (
+  getSettingsFn: (categoryName: string) => ISettingsSubCategory[]
+) => {
+  return [
+    getSettingsFn('Stream').map(hideParamsForCategory),
+    getSettingsFn('Output').map(hideParamsForCategory)
+  ];
+};
+
+const paramsToShow = ['server', 'VBitrate', 'ABitrate'];
+
+const hideParamsForCategory = (
+  category: ISettingsSubCategory
+): ISettingsSubCategory => ({
+  ...category,
+  parameters: hideParams(category.parameters)
+});
+
+const hideParams = (parameters: TObsFormData): TObsFormData => {
+  return parameters.map(parameter => ({
+    ...parameter,
+    visible: paramsToShow.includes(parameter.name)
+  }));
+};
